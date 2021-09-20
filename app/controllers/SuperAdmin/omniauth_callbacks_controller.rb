428b725d00e63e88926544fbb962788b694e6e69
Coder1221 <21100328@lums.edu.pk>
2021-09-20 16:23:02 +0500

    model_callback;

--- app/controllers/SuperAdmin/omniauth_callbacks_controller.rb
+++ app/controllers/SuperAdmin/omniauth_callbacks_controller.rb
@@ -7,38 +7,19 @@ class SuperAdmin::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
   def google_oauth2
     handle_auth "Google"
   end
-  
+
   def fb_auth
     handle_auth "Facebook"
   end
 
-
   def handle_auth(kind)
-    @super_admin = SuperAdmin.from_omniauth(request.env['omniauth.auth'])
+    @super_admin = SuperAdmin.from_omniauth(request.env["omniauth.auth"])
     if @super_admin.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
       sign_in_and_redirect @super_admin, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      session["devise.auth_data"] = request.env["omniauth.auth"].except("extra") # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @super_admin.errors.full_messages.join("\n")
     end
   end
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -2,7 +2,7 @@ class SuperAdmin < ApplicationRecord
   rolify
   resourcify
   attr_accessor :invited_by_role, :schoolname #virtual attribute
-  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable ,:omniauthable ,omniauth_providers: [:google_oauth2 , :fb_auth]
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :fb_auth]
   belongs_to :school
 
   enum status: {
@@ -14,4 +14,18 @@ class SuperAdmin < ApplicationRecord
     roles.first.name.to_sym
   end
 
+  def self.from_omniauth(access_tocken)
+    super_admin = SuperAdmin.where(email: access_tocken.info.email).first
+    unless super_admin
+      super_admin = SuperAdmin.create(
+        email: access_tocken.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+      @schl = School.create(:name => 'Provider_created')
+      super_admin.school = @schl
+      super_admin.name = access_tocken.info.name
+      super_admin.save
+    end
+    super_admin
+  end
 end
