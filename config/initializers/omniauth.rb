80d17fbc169ab3af858408bf74388323737a695f
Coder1221 <21100328@lums.edu.pk>
2021-09-20 11:45:05 +0500

    env renamed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2 , ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+    provider :google_oauth2 , ENV['G_CLIENT_ID'], ENV['G_CLIENT_SEC']
 end
 OmniAuth.config.allowed_request_methods = %i[post]
