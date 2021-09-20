1b7ddbd288c17505ba054dbd25a84cf6dd433fe5
Coder1221 <21100328@lums.edu.pk>
2021-09-20 18:59:23 +0500

    fixing

--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -15,17 +15,23 @@ class SuperAdmin < ApplicationRecord
   end
 
   def self.from_omniauth(access_tocken)
-    super_admin = SuperAdmin.where(email: access_tocken.info.email).first
-    unless super_admin
-      super_admin = SuperAdmin.create(
-        email: access_tocken.info.email,
-        password: Devise.friendly_token[0, 20]
-      )
-      @schl = School.create(:name => 'Provider_created')
-      super_admin.school = @schl
-      super_admin.name = access_tocken.info.name
-      super_admin.save
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
     end
-    super_admin
   end
+
+    # super_admin = SuperAdmin.where(email: access_tocken.info.email).first
+    # unless super_admin
+    #   super_admin = SuperAdmin.create(
+    #     email: access_tocken.info.email,
+    #     password: Devise.friendly_token[0, 20]
+    #   )
+    #   @schl = School.create(:name => 'Provider_created')
+    #   super_admin.school = @schl
+    #   super_admin.name = access_tocken.info.name
+    #   super_admin.save
+    # end
+    # super_admin
 end
