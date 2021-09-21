32edbe33c80ff97b0c5ff7b13a28696821243804
Coder1221 <21100328@lums.edu.pk>
2021-09-21 14:40:38 +0500

    Email problem for fb provider

--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -15,28 +15,47 @@ class SuperAdmin < ApplicationRecord
   end
 
   def self.from_omniauth(access_tocken)
-
-    puts access_tocken ,'<------------------------------------------------------------------------------------------------------------------------>' 
-    super_admin = SuperAdmin.where(email: access_tocken.info.email).first
-
-    unless super_admin
-      @super_admin = SuperAdmin.create(
-        email: access_tocken.info.email,
-        password: Devise.friendly_token[0, 20],
-        provider: access_tocken.provider,
-        uid: access_tocken.uid,
-        name: access_tocken.info.name
-      )
-      @schl = School.create(:name => 'Provider_created')
-      @super_admin.school = @schl
-      @super_admin.save
-      
-      if @super_admin.persisted?
-        @super_admin.add_role(:super_admin)
+    puts '<------------------------------------------------------------------------------------------------------------------------>' 
+    puts access_tocken 
+    puts '<------------------------------------------------------------------------------------------------------------------------>' 
+    if access_tocken.provider == 'facebook'
+      @super_admin = SuperAdmin.where(uid: access_tocken.uid).first
+      unless @super_admin
+        @temp_email = access_tocken.info.name.gsub(' ','_') + access_tocken.uid.to_s + '@gmail.com'
+        @super_admin = SuperAdmin.create(
+          email: @temp_email,
+          password: Devise.friendly_token[0,20],
+          provider: access_tocken.provider,
+          uid: access_tocken.uid,
+          name: access_tocken.info.name
+        )
+        @schl = School.create(:name => 'FB_Provider_created')
+        @super_admin.school = @schl
+        @super_admin.save
+        if @super_admin.persisted?
+          @super_admin.add_role(:super_admin)
+        end
       end
+    else
+      @super_admin = SuperAdmin.where(email: access_tocken.info.email).first
+      unless @super_admin
+        @super_admin = SuperAdmin.create(
+          email: access_tocken.info.email,
+          password: Devise.friendly_token[0, 20],
+          provider: access_tocken.provider,
+          uid: access_tocken.uid,
+          name: access_tocken.info.name
+        )
+        @schl = School.create(:name => 'Provider_created')
+        @super_admin.school = @schl
+        @super_admin.save
+        
+        if @super_admin.persisted?
+          @super_admin.add_role(:super_admin)
+        end
 
+      end
     end
     @super_admin
   end
-
 end
