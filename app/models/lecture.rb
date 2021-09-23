f60d40a5c22d9e8df2e5a66718b64bee13c2f8f5
Coder1221 <21100328@lums.edu.pk>
2021-09-23 13:08:37 +0500

    Working cocoon gem commit

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,5 @@
 class Lecture < ApplicationRecord
     belongs_to :super_admin
-    has_many :lecture_contents ,depenedent: :destroy
-    accept_nested_attributes_for :lecture_contents ,reject_if: :all_blank , allow_destroy: ture
+    has_many :lecture_contents ,dependent: :destroy
+    accepts_nested_attributes_for :lecture_contents , reject_if: :all_blank , allow_destroy: true
 end
