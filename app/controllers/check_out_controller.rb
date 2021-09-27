4a0792953593da3c28fa3d9fccc9259e6fa90cf2
Awesome Code <team@xinminlabs.com>
2021-09-27 11:12:58 +0000

    Auto corrected by following Format Ruby Code

--- app/controllers/check_out_controller.rb
+++ app/controllers/check_out_controller.rb
@@ -1,19 +1,18 @@
 class CheckOutController < ApplicationController
-  
   def create
-    @session = Stripe::Checkout::Session.create({
-      customer: current_user.stripe_customer_id,
-      success_url: 'https://musifyschool.herokuapp.com/lectures',
-      cancel_url:  'https://musifyschool.herokuapp.com/lectures',
-      payment_method_types: ['card'],
-      line_items: [
-        {price: params[:price] , quantity: 1},
-    ],
-      mode: 'payment',
-    })
+    @session =
+      Stripe::Checkout::Session.create(
+        {
+          customer: current_user.stripe_customer_id,
+          success_url: 'https://musifyschool.herokuapp.com/lectures',
+          cancel_url: 'https://musifyschool.herokuapp.com/lectures',
+          payment_method_types: ['card'],
+          line_items: [{ price: params[:price], quantity: 1 }],
+          mode: 'payment'
+        }
+      )
     respond_to do |format|
       format.js
     end
   end
-
 end
