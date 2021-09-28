76c1dee8bc609c7703c5eec43e37c0aafe05c8ba
Coder1221 <21100328@lums.edu.pk>
2021-09-28 14:22:05 +0500

    Merged temp2 branch of stripe

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'image_processing'
 gem "rails_best_practices"
 gem "solargraph"
 gem 'stripe'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/check_out_controller.rb
+++ app/controllers/check_out_controller.rb
@@ -2,15 +2,16 @@ class CheckOutController < ApplicationController
   
   def create
     @session = Stripe::Checkout::Session.create({
-      customer: current_user.stripe_customer_id,
+      customer_email: current_user.email,
       success_url: 'https://musifyschool.herokuapp.com/lectures',
-      cancel_url:  'https://musifyschool.herokuapp.com/lectures',
+      cancel_url:  'https://musifyschool.herokuapp.com/422',
       payment_method_types: ['card'],
       line_items: [
         {price: params[:price] , quantity: 1},
-    ],
+      ],
       mode: 'payment',
     })
+    puts @session ,'-------------------------------------------------------------->'
     respond_to do |format|
       format.js
     end
