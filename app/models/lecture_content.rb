075a855396e862361fc91a280c5d36add714f63f
Coder1221 <21100328@lums.edu.pk>
2021-09-29 19:14:54 +0500

    validations added for lecture content

--- app/models/lecture_content.rb
+++ app/models/lecture_content.rb
@@ -1,4 +1,6 @@
 class LectureContent < ApplicationRecord
     belongs_to  :lecture
     has_many_attached :pdfs ,dependent: :destroy
+    validates :desciption , presence: true
+    validates :content , presence: true
 end
